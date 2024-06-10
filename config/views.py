7f050e60c3f2115ac8b9753cee67100b4dba67bd
_dlwogur_ <jaehyuk712@gmail.com>
2024-06-10 22:04:17 +0900

    feat: add Feed object

--- config/views.py
+++ config/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from content.models import Feed
 
 class Main(APIView):
     def get(self, request):
-        return render(request, 'insta/main.html')
+        '''
+        만든 모델 활용하는 코드
+
+        object 붗여서 모델 안 데이터를 다룰 수 있다.
+
+        => feed_list에 Feed라는 모델 안에 있는 모든 데이터를 넣는 코드
+
+        데이터가 여러개라면 List 형태로 만들어짐
+        feed_list = [Feed1, Feed2, Feed3, ...]
+        '''        
+        feed_list = Feed.objects.all()
+
+        # key = feed_list, value = feed_list
+        return render(request, 'insta/main.html', context=dict(feed_list=feed_list))
     
